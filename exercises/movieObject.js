41b596e88c62633275b9de687e4f7d1892e29363
cslunetta <cslunetta@outlook.com>
2021-07-06 14:01:10 -0500

    add movie object challenge

---
+++ exercises/movieObject.js
@@ -0,0 +1,12 @@
+/* 
+movie object that takes in 5 arguements
+
+Title
+Director
+Genre
+Release year
+Rating
+
+getoverview
+    "<movie>, a <genre> film directed by <director> was released in <releaseYear>. It received a rating of <rating>."
+*/
