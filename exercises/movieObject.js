b250b157dd616e84941b3e4bb8b7836f85add0e4
cslunetta <cslunetta@outlook.com>
2021-07-06 14:39:42 -0500

    My answer to Movie Object challenge

--- exercises/movieObject.js
+++ exercises/movieObject.js
@@ -10,3 +10,53 @@ Rating
 getoverview
     "<movie>, a <genre> film directed by <director> was released in <releaseYear>. It received a rating of <rating>."
 */
+
+// function Movie(title, director, genre, releaseYear, rating) {
+//     this.title = title;
+//     this.director = director;
+//     this.genre = genre;
+//     this.releaseYear = releaseYear;
+//     this.rating = rating;
+
+//     Movie.prototype.getOverview = () => {
+//         return `${title}, a ${genre} film directed by ${director} was released in ${releaseYear}. It received a rating of ${rating}.`;
+//     };
+// }
+
+class Movie {
+    constructor(title, director, genre, releaseYear, rating) {
+        this.title = title;
+        this.director = director;
+        this.genre = genre;
+        this.releaseYear = releaseYear;
+        this.rating = rating;
+    }
+
+    get overview() {
+        return movieObjectResults.getOverview();
+    }
+
+    getOverview = () => {
+        return `${this.title}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}.`;
+    };
+}
+
+export const movieObjectResults = () => {
+    const newMovie = new Movie(
+        "untitled movie",
+        "me myself and I",
+        "questionable",
+        2021,
+        4.5
+    );
+    console.log(newMovie.getOverview());
+
+    const anotherMovie = new Movie(
+        "movie123",
+        "Who knows?",
+        "also questionable",
+        2021,
+        2
+    );
+    console.log(anotherMovie.getOverview());
+};
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import { animalResults } from "./exercises/animals.js";
 import { availableBookResults } from "./exercises/availableBooks.js";
+import { movieObjectResults } from "./exercises/movieObject.js";
 
 animalResults();
 availableBookResults();
+movieObjectResults();
